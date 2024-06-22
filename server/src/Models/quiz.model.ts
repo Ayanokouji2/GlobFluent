5238b3d3ef98e9a1b86fa2d6e5f657a92d04137a
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-06-22 07:42:43 +0530

    chore: Add genre field to Quiz model

--- server/src/Models/quiz.model.ts
+++ server/src/Models/quiz.model.ts
@@ -1,30 +1,36 @@
 import { Schema, model, Document } from 'mongoose'
 import { Question } from './question.model'
 
-export interface Quiz extends Document{
-    title : string
-    questions : Question[] // leaving it for discussion
-    marks : number
+export interface Quiz extends Document {
+    title: string
+    questions: Question[] // leaving it for discussion
+    marks: number
+    genre: string
 }
 
 
-const quizSchema : Schema<Quiz> = new Schema({
-    title:{
-        type : String,
-        required : true,
-        trim : true
+const quizSchema: Schema<Quiz> = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
     },
-    marks:{
-        type : Number,
-        required : true         
+    marks: {
+        type: Number,
+        required: true
     },
-    questions : {
-        type : [Schema.Types.ObjectId],
-        ref : "Question",
-        required : true
+    questions: {
+        type: [Schema.Types.ObjectId],
+        ref: "Question",
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true,
+        trim: true
     }
 
 })
 
-const quizModel = model<Quiz>( 'Quiz', quizSchema )
+const quizModel = model<Quiz>('Quiz', quizSchema)
 export default quizModel
--- server/what_To_do.md
+++ server/what_To_do.md
@@ -5,5 +5,10 @@
 /quiz/:id ✅
 
 <!-- create quiz -->
-take the title and all the question with options and the correct options 
+take the title and all the question with options and the correct options  ✅
+
+<!-- Update Quiz -->
+Optional
+
+<!-- Route when user submits the quiz to generate the result and save it in the database -->
 
